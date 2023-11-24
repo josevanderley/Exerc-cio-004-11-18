337dfe6cf9a5ecc1a0163cd1511375163f989aad
josevanderley <89553942+josevanderley@users.noreply.github.com>
2023-11-24 19:41:04 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_004_18_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a senha: ");
+            string senhaUsuario = Console.ReadLine();
+
+            if (senhaUsuario == "1234")
+            {
+                Console.WriteLine("ACESSO PERMITIDO");
+            }
+            else
+            {
+                Console.WriteLine("ACESSO NEGADO");
+                {
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
+    
